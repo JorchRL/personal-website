f052f6b952fded3a5963e0de28b1d61ee6c3438c
Jorge Romero Lagunas <jorchrl@Jorges-MacBook-Pro.local>
2022-04-14 18:51:08 -0500

    Add alt prop to image

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       <article className="flex-1 px-9 mt-10 flex justify-start items-center">
         <div className="container self-start">
           <h1 className="font-bold text-2xl">
-            Hi! I'm Jorge and I build stuff with code.{" "}
+            Hi! {`I'm`} Jorge and I build stuff with code.{" "}
           </h1>
           <p>My site is currently under construction. Hire me? </p>
         </div>
@@ -19,12 +19,16 @@ const Home: NextPage = () => {
             In the meantime, why not check out my blog on Hashnode?
           </h2>
           <button className=" text-white hover:text-blue-900 hover:bg-slate-300 font-bold bg-slate-500 rounded-full py-3 px-3">
-            <a href="https://jorgerl.hashnode.dev">Jorch's Mini Blog</a>
+            <a href="https://jorgerl.hashnode.dev">{`Jorch's Mini Blog`}</a>
           </button>
         </div>
       </article>
-      <footer className="">
-        <Image src={footerPic} layout="responsive" />
+      <footer>
+        <Image
+          src={footerPic}
+          layout="responsive"
+          alt="drawing of my blog's avatar"
+        />
       </footer>
     </div>
   );
