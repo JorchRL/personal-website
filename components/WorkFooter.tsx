cb148000438e96ce5d565bd2de2299b0c563fc6f
Jorge Romero <jorch.rl@gmail.com>
2022-12-06 21:51:39 -0600

    Update WorkFooter.tsx

--- components/WorkFooter.tsx
+++ components/WorkFooter.tsx
@@ -4,7 +4,7 @@ const WorkFooter = () => {
     return (
         <div className="bg-blue-500 w-screen h-20 flex justify-center items-center"    >
             <h2 className="text-white text-2xl py-3">
-                See my work
+                
             </h2>
         </div>
     )
